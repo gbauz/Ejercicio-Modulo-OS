2d0a7f6a9cd23a4f771fcf6c00c61a6b010532c2
Giovanni Bauz <92232987+gbauz@users.noreply.github.com>
2024-02-11 14:43:35 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-
+const os=require('os');
+console.log('Tipo de sistema operativo:',os.type());
+console.log('version del sistema operativo',os.release());
+console.log('numero total de nucleos del cpu',os.cpus().length);
+console.log('Memoria libre del sistema en bystes',os.totalmem());
